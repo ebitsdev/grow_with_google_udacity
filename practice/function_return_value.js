fb897e1b65b1e8a7ac019a39e368369503e0b501
ebitsdev <ebambadev@ebamba.com>
2018-01-23 17:25:40 -0500

    function return value

---
+++ practice/function_return_value.js
@@ -0,0 +1,23 @@
+// Function to add numbers
+function add(x, y) {
+    return x + y;
+}
+
+// Function to divise numbers
+
+function divideByTwo(num){
+    return num / 2;
+}
+// Use the different functions discussed above
+var sum = add(15, 67);
+var average = divideByTwo(sum);
+
+console.log(average);
+function addTen(x){
+    return x + 10;
+}
+function divideByThree(y){
+    return y / 3;
+}
+var result = addTen(2);
+console.log(divideByThree(result)); // Produces 4
