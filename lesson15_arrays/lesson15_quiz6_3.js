ed6c40897c910adc0e431e15c7db10de5943d680
ebitsdev <ebambadev@ebamba.com>
2018-01-25 13:50:16 -0500

    Started lesson 15

---
+++ lesson15_arrays/lesson15_quiz6_3.js
@@ -0,0 +1,13 @@
+/*
+Quiz: The Price is Right (6-3)
+Directions:
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+*/
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+/* TIP: The 1st element of any array has an index of 0.
+Afterwards, print out the prices array to the console.*/
+prices[0] = 1.25;
+prices[2] = 100.00;
+prices[6] = 200.01;
+
+console.log(prices);
