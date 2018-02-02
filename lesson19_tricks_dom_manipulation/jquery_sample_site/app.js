011ef792b97e8250cbfa7265cfd7b0b940dd899a
ebitsdev <>
2018-02-01 21:32:38 -0500

    Completed lesson 19

---
+++ lesson19_tricks_dom_manipulation/jquery_sample_site/app.js
@@ -0,0 +1,24 @@
+/*
+
+For this quiz, can you use this script, which is linked in the <head> of index.html,
+to change the boring placeholder image to a picture of a cute animal?
+
+Remember, you'll need to pass a function into the jQuery object to run
+when the document is ready.
+
+Unfortunately, placepuppy is no longer available. Here's a link to a random
+animal image on lorempixel.com:
+http://lorempixel.com/350/150/animals/
+
+Good luck!
+
+*/
+function changeImgSrc(){
+    var header = "Some Heading String"
+    $("#nav h1").html(header);
+    var imgsrc = $(".article-item img");
+    imgsrc.attr("src", "http://placekitten.com/350/150/"); //Replace the source of the image placeholder
+ }
+$(function(){
+    changeImgSrc(); // Call the changeImgSrc() function
+})
