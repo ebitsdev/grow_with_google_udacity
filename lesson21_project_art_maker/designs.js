8cc2b1d3efa79d348d2974b9c4a076a6ab53cb30
ebitsdev <ebambadev@ebamba.com>
2018-02-09 13:12:58 -0500

    Completed Pixel Art maker project

--- lesson21_project_art_maker/designs.js
+++ lesson21_project_art_maker/designs.js
@@ -1,11 +1,10 @@
-// Inspiration from: https://stackoverflow.com/questions/16001685/manipulate-css-with-the-html5-color-input
 function makeTable(table, tr, td){
-    /* Create the table element */
-    table = $('<table/>');
+    /* Create the table element using nested for loop */
+    table = $('<table/>', {class: 'table text-center table-bordered'});
     /* Loop over the tr */
     for (let i = 0; i < tr; i++){
          /* Create individual row */
-        let row = $('<tr/>');
+        let row = $('<tr/>', {scope: 'row'});
         /* Loop over the td */
         for(let j = 0; j < td; j++){
             /* Create individual cell */
@@ -24,19 +23,30 @@ function makeTable(table, tr, td){
 }
 
 function makeGrid(){
+    /* Use .submit() event to dynamically generate the table */
     $('#sizePicker').submit(function(event){
         /* Get the values of the input fields to use in generating the grid */
         let tr = $('#inputHeight').val();
         let td = $('#inputWidth').val();
+        /* Prevent default behavior of submit */
         event.preventDefault();
+        /* Use Bootstrap color picker */
+        $('#colorPicker').colorpicker({"color": "dodgerblue"});
         /* Generate the grid by calling jQuery .html() and the makeTable() function */
         $('#pixelCanvas').html(makeTable(null, tr, td));
-    }).submit(); /* To invoke makeGrid by calling submit() immediately */
+    }).submit(); /* Invoke submit() immediately */
 }
-/* To automatically call the makeGrid function when the document is ready */
+/* Call makeGrid() when the document is ready */
 
 $(function() {
-    /* Call makeGrid on document ready */
     makeGrid();
 });
-/** More comments */
+/**
+ * ==================
+ * Notes
+ * ==================
+ *
+ * In this project I learned how to dynamically generate a grid (table) of any size using for loop
+ * Used jQuery event listeners to set the clicked cell color
+ * Used Bootstrap framework to style the form elements and other html component on the page
+ * **/
